8b6e94a026434b1490f28d87d84da926a7e5dc96
SK Abang Gesa <sekolah-8368@moe-dl.edu.my>
2025-12-01 19:33:22 +0800

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,6 @@
+// js/firebase.config.js
+
+// Mengimport rujukan dari objek global 'window' yang ditakrifkan dalam index.html
+export const db = window.db;
+export const auth = window.auth;
+export const storage = window.storage;
