36df187474fd9b45fffc3370bb871e46adf3ab67
SK Abang Gesa <sekolah-8368@moe-dl.edu.my>
2025-12-01 19:38:10 +0800

    Update firebase.config.js

--- js/firebase.config.js
+++ js/firebase.config.js
@@ -1,6 +1,15 @@
+[file name]: firebase-config.js
+[file content begin]
 // js/firebase.config.js
 
 // Mengimport rujukan dari objek global 'window' yang ditakrifkan dalam index.html
-export const db = window.db;
-export const auth = window.auth;
-export const storage = window.storage;
+// Tambahkan pengecekan untuk memastikan objek tersedia
+export const db = window.db || null;
+export const auth = window.auth || null;
+export const storage = window.storage || null;
+
+// Fungsi untuk memastikan Firebase sudah siap
+export const isFirebaseReady = () => {
+    return window.db && window.auth && window.storage;
+};
+[file content end]
