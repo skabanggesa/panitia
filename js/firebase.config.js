851d74b347df101a1391e9373b8b5714527e4604
SK Abang Gesa <sekolah-8368@moe-dl.edu.my>
2025-12-01 19:34:09 +0800

    Delete js/firebase.config.js

--- js/firebase.config.js
+++
@@ -1,6 +0,0 @@
-// js/firebase.config.js
-
-// Mengimport rujukan dari objek global 'window' yang ditakrifkan dalam index.html
-export const db = window.db;
-export const auth = window.auth;
-export const storage = window.storage;
