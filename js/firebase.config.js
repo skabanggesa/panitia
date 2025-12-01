de716a44e35d9d6eec4f1ce946af8aa890f3f4b5
SK Abang Gesa <sekolah-8368@moe-dl.edu.my>
2025-12-01 19:30:43 +0800

    Update firebase.config.js

--- js/firebase.config.js
+++ js/firebase.config.js
@@ -1,7 +1,6 @@
 // js/firebase.config.js
 
-// Import rujukan dari objek global 'window' yang telah ditetapkan dalam index.html
+// Mengimport rujukan dari objek global 'window' yang ditakrifkan dalam index.html
 export const db = window.db;
-// TIDAK PERLU EKSPORT 'auth' LAGI, kita akan aksesnya secara langsung dalam app.js
-export const auth = window.auth; 
+export const auth = window.auth;
 export const storage = window.storage;
